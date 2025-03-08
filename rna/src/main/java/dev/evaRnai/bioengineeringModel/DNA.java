31e6104e6ab4c9b5e521bf0eb05c7e892e800cae
Emagmunioz <emagmunioz@gmail.com>
2025-03-08 22:57:04 +0100

    test tun ok

--- rna/src/main/java/dev/evaRnai/bioengineeringModel/DNA.java
+++ rna/src/main/java/dev/evaRnai/bioengineeringModel/DNA.java
@@ -1,5 +1,18 @@
 package dev.evaRnai.bioengineeringModel;
 
 public class DNA {
+    private String sequence;
+
+    public DNA(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
 
 }
--- rna/src/test/java/dev/evaRnaiTest/bioengineeringModelTest/DNATest.java
+++ rna/src/test/java/dev/evaRnaiTest/bioengineeringModelTest/DNATest.java
@@ -1,8 +1,8 @@
 package dev.evaRnaiTest.bioengineeringModelTest;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
 import org.junit.jupiter.api.Test;
+import dev.evaRnai.bioengineeringModel.DNA;
 
 public class DNATest {
     @Test
