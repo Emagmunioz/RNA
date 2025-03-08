2e6d881e15204a538003eb4fd61db9760a6c2015
Emagmunioz <emagmunioz@gmail.com>
2025-03-08 22:50:30 +0100

    Test getSequence() and setSequence() methods

--- rna/src/test/java/dev/evaRnaiTest/bioengineeringModelTest/DNATest.java
+++ rna/src/test/java/dev/evaRnaiTest/bioengineeringModelTest/DNATest.java
@@ -1,5 +1,20 @@
 package dev.evaRnaiTest.bioengineeringModelTest;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
 
 public class DNATest {
+    @Test
+    public void testGetSequence() {
+        DNA dna = new DNA("GCTA");
+        assertThat(dna.getSequence(), is("GCTA"));
+    }
+    @Test
+    public void testSetSequence() {
+        DNA dna = new DNA("");
+        dna.setSequence("GCTA");
+        assertThat(dna.getSequence(), is("GCTA"));
+    }   
 
 }
