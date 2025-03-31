b2ba6d064ab3b56201313fb0321ea77a5ea63693
Emagmunioz <emagmunioz@gmail.com>
2025-03-31 12:13:20 +0200

    GREEN: Implement Nucleotide enum with basic functionality

---
+++ rna/src/main/java/dev/Nucleotide.java
@@ -0,0 +1,24 @@
+package dev;
+
+public enum Nucleotide {
+
+    ADENINE('A', 'U'),
+    CYTOSINE('C', 'G'),
+    GUANINE('G', 'C'),
+    THYMINE('T', 'A');
+
+    private final char dnaSymbol;
+    private final char rnaComplement;
+    
+    Nucleotide(char dnaSymbol, char rnaComplement) {
+        this.dnaSymbol = dnaSymbol;
+        this.rnaComplement = rnaComplement;
+    }
+    public static char getRNAComplement(char dnaSymbol) {
+        for (Nucleotide n : values()) {
+            if (n.dnaSymbol == dnaSymbol) {
+                return n.rnaComplement;
+            }
+        }
+}
+}
--- rna/src/test/java/dev/NucleotideTest.java
+++ rna/src/test/java/dev/NucleotideTest.java
@@ -1,7 +1,7 @@
 package dev;
-import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.Test;
 public class NucleotideTest {
     
     @Test
