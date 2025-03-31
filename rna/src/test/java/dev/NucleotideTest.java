a6c440b223af9ec6c5984eab515d7c302011a343
Emagmunioz <emagmunioz@gmail.com>
2025-03-31 12:21:33 +0200

    Add other  nucleotide tests

--- rna/src/test/java/dev/NucleotideTest.java
+++ rna/src/test/java/dev/NucleotideTest.java
@@ -8,6 +8,23 @@ public class NucleotideTest {
     public void whenAdenine_thenReturnUracil() {
         char complement = Nucleotide.getRNAComplement('A');
         assertThat(complement, is('U'));
-    }   
+    }  
+    @Test
+    public void whenCytosine_thenReturnGuanine() {
+        char complement = Nucleotide.getRNAComplement('C');
+        assertThat(complement, is('G'));
+    }
+    @Test
+    public void whenGuanine_thenReturnCytosine() {
+        char complement = Nucleotide.getRNAComplement('G');
+        assertThat(complement, is('C'));
+    }
+
+    @Test
+    public void whenThymine_thenReturnAdenine() {
+        char complement = Nucleotide.getRNAComplement('T');
+        assertThat(complement, is('A'));
+    }
+
 
 }
