95b888e38b22c2a8b02cf2f980b77c7534bb0694
Emagmunioz <emagmunioz@gmail.com>
2025-03-31 10:27:05 +0200

    GREEN: Implement minimal solution for empty DNA sequence

---
+++ rna/src/main/java/dev/DNATranscriber.java
@@ -0,0 +1,8 @@
+package dev;
+
+public class DNATranscriber {
+    public String transcribe(String dna) {
+        return "";
+    }
+
+}
--- rna/src/test/java/DNATranscriberTest.java
+++ rna/src/test/java/DNATranscriberTest.java
@@ -1,8 +1,10 @@
 
-
-import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+import dev.DNATranscriber;
 
 public class DNATranscriberTest {
     
