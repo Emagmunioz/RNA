0840bb6f32bfec849ee22d0a62e799cac59bdcc0
Emagmunioz <emagmunioz@gmail.com>
2025-03-31 11:50:29 +0200

    GREEN: Implement hardcoded solution for G nucleotide

--- rna/src/main/java/dev/DNATranscriber.java
+++ rna/src/main/java/dev/DNATranscriber.java
@@ -4,5 +4,9 @@ public class DNATranscriber {
     public String transcribe(String dna) {
         return "";
     }
+public String transcribe(String dna) {
+    if (dna.isEmpty()) return "";
+    return "C";
+    }   
 
 }
