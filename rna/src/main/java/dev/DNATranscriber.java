43fe198488f754e60704f9ee636211fd9c9f81c5
Emagmunioz <emagmunioz@gmail.com>
2025-03-31 11:53:12 +0200

    GREEN: Implement transcription for G and C nucleotides

--- rna/src/main/java/dev/DNATranscriber.java
+++ rna/src/main/java/dev/DNATranscriber.java
@@ -8,5 +8,12 @@ public String transcribe(String dna) {
     if (dna.isEmpty()) return "";
     return "C";
     }   
-
+public String transcribe(String dna) {
+    if (dna.isEmpty()) return "";
+        
+    char nucleotide = dna.charAt(0);
+    if (nucleotide == 'G') return "C";
+    if (nucleotide == 'C') return "G";
+    return "";
+    }
 }
